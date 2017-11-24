13a7f26f0c3ff2582a2e5c65e8b62fc450db19e7
ilanrosc <33714194+ilanrosc@users.noreply.github.com>
2017-11-24 11:57:51 +0200

    complete.R and pollutantmean.R
    complete.R and pollutantmean.R

---
+++ complete.R
@@ -0,0 +1,24 @@
+complete <- function(directory, id = 1:332) {
+        setwd(paste(getwd(), "/", directory, sep = ""))
+        # list all files in the directory dir()
+        filename <- dir()
+        #Load all fileas as a list
+        
+        filelist <- lapply(filename, read.csv)
+        
+        res <- data.frame()
+        
+        for (i in id) {
+                
+                x <- data.frame(filelist[[i]])
+                g <- complete.cases(x)
+                x <- x[g, ]
+                nobs <- length(x[,1])
+                y <- data.frame("id" = i, "nobs" = nobs)
+                res <- rbind(res, y)
+                
+        }
+        
+        res[complete.cases(res), ]
+}
+
---
+++ pollutantmean.R
@@ -0,0 +1,23 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+        setwd(paste(getwd(), "/", directory, sep = ""))
+        # list all files in the directory dir()
+        filename <- dir()
+        #Load all fileas as a list
+        
+        filelist <- lapply(filename, read.csv)
+        
+        y <- data.frame()
+        
+        for (i in id) {
+                
+                x <- if (pollutant == "sulfate") {filelist[[i]][2]} else filelist[[i]][3]
+                
+                # x <- filelist[[1]][2]
+                x1 <- data.frame(x, check.names = FALSE)
+                y <- rbind(y,x1, deparse.level = 0)
+                
+        }
+        
+        bad <- is.na(y[1])
+        mean(y[!bad])
+}
